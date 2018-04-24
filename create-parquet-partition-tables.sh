fd31d8115e4c7a1f97556240162ae41c0556c5e8
Kent Yao <11215016@zju.edu.cn>
2018-04-24 17:17:57 +0800

    use spark-sql to create hive partition tables

--- create-parquet-partition-tables.sh
+++ create-parquet-partition-tables.sh
@@ -4,5 +4,5 @@ bin=`cd $bin;pwd`
 
 source $bin/tpcds-env.sh
 
-hive -f $bin/create-table-sql/create-load-gzip-parquet-dimension.sql
-hive -f $bin/create-table-sql/create-load-gzip-parquet-partition-fact.sql
+$SPARK_HOME/bin/spark-sql $@ -f $bin/create-table-sql/create-load-gzip-parquet-dimension.sql
+$SPARK_HOME/bin/spark-sql $@ -f $bin/create-table-sql/create-load-gzip-parquet-partition-fact.sql
